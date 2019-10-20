285a2cb59083fae6a1179c77190b403ff7a90c81
Dishant Janakbhai Patel <39313255+dishantpatel98@users.noreply.github.com>
2019-10-20 16:49:22 +0530

    Update SPI_CORTEX_2.c

--- SPI_CORTEX_2.c
+++ SPI_CORTEX_2.c
@@ -1,44 +1,37 @@
-#include "lpc17xx.h"
-#include "system_LPC17xx.h"
+#include<lpc17xx.h>
 
-int main(void)
+void spi_ini()
 {
-	int i, j;
-	LPC_PINCON->PINSEL0 |= (1<<31)|(1<<30);//clock for 
-	LPC_PINCON->PINSEL1 |= (1<<5)|(1<<4);//mosi
-	LPC_GPIO0->FIODIR2 |= (1<<0);
-/* IN  lpc2148 we have 60mhz........ and in lpc1768 we have 100mHZ 
-	-->For each and every block we can set different frequency
-	*/
-	
-	
-	LPC_SPI->SPCR = 0x00;  //SPCR=SPI CONTROL REGISTER
-	LPC_SPI->SPCR |= (1<<5);  //MASTER Mode
-	LPC_SPI->SPCCR	 = 0x10;   // THis Register indicates that master pf SPI Peripheral clock cycles that makes up an spi clock
+	LPC_GPIO0->FIODIR2|=(1<<0);																				// Set the SSEL bit as output.
+	LPC_PINCON->PINSEL0|=(1<<30)|(1<<31);
+	LPC_PINCON->PINSEL1|=(1<<4)|(1<<5);
+	LPC_SPI->SPCR|=(1<<5)|(1<<6)|(1<<11);
+	LPC_SPI->SPCCR=0X10;
+}
 
+void delay(unsigned int de)
+{
+	for(int i=0;i<=de;i++)
+	{
+		for(int j=0;j<=de;j++);
+	}
+}
+
+int main()
+{
+	spi_ini();
 	while(1)
 	{
+	LPC_GPIO0->FIOSET2|=(1<<0);
+	LPC_SPI->SPDR=0XAA;
+	while(!(LPC_SPI->SPSR & (1<<7)));
+	delay(10000);
+	LPC_GPIO0->FIOCLR2|=(1<<0);
 		
-    //LPC_GPIO0->FIOCLR2 |= (1<<0);
-		LPC_GPIO0->FIOSET2 |= (1<<0);     //HIGH PULSE
-		LPC_SPI->SPDR = 0xff;
-
-		while(!(LPC_SPI->SPSR)&(1<<7));//SPSR=SPI STATUS REGISTER
-
-		for(i=0;i<10000;i++)
-			for(j=0;j<50;j++);
-
-		LPC_GPIO0->FIOCLR2 |= (1<<0); //PULSE AGAIN AFTER DATA
-    LPC_GPIO0->FIOSET2 |= (1<<0);
-	   	LPC_SPI->SPDR = 0x00;
-
-
-		while(!(LPC_SPI->SPSR)&(1<<7));		
-
-		for(i=0;i<10000;i++)
-			for(j=0;j<50000;j++);
-
-		LPC_GPIO0->FIOCLR2 |= (1<<0);
-	  //LPC_GPIO0->FIOSET2 |= (1<<0);     HIGH PULSE
+	LPC_GPIO0->FIOSET2|=(1<<0);
+	LPC_SPI->SPDR=0X55;
+	while(!(LPC_SPI->SPSR & (1<<7)));
+	delay(10000);
+	LPC_GPIO0->FIOCLR2|=(1<<0);
 	}
 }
